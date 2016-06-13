0b2e2bf174116067cd4132646e1308c626ce2316
nenad <neno.nenadovic@gmail.com>
2016-06-13 16:53:04 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+This project shows basic configuration techniques
+
+You can start it with spring CLI:
+spring 
---
+++ src/test/java/net/demo/BasicConfigurationApplicationTests.java
@@ -0,0 +1,16 @@
+package net.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BasicConfigurationApplication.class)
+public class BasicConfigurationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
