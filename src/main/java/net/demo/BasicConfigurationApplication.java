56056603f1e2047545604f18a84cfa36d36234a3
nenad <neno.nenadovic@gmail.com>
2016-06-19 23:52:20 +0200

    add beans to application context using different techniques.

--- src/main/java/net/demo/BasicConfigurationApplication.java
+++ src/main/java/net/demo/BasicConfigurationApplication.java
@@ -3,14 +3,17 @@ package net.demo;
 import net.demo.pojo.SimplePojo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import static java.lang.System.out;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class BasicConfigurationApplication {
 
 	public static void main(String[] args) {
@@ -21,14 +24,36 @@ public class BasicConfigurationApplication {
 		}
 
 		BasicConfigurationApplication app = new BasicConfigurationApplication();
-		out.println(app.simplePojo);
+		app.printFields();
 	}
 
 	@Bean
-	SimplePojo anotherPojo() {
-		return new SimplePojo();
+	public SimplePojo createdByPrototypeFactory() {
+		return new SimplePojo("createdByPrototypeFactory");
+	}
+
+	@Bean
+	public static SimplePojo createdByStaticFactory() {
+		return new SimplePojo("createdByStaticFactory");
 	}
 
 	@Autowired
-	private SimplePojo simplePojo;
+	private SimplePojo namedInAnnotation;
+
+	private SimplePojo simplePojo = new SimplePojo("created by calling custom constructor");
+
+	//@Autowired
+	//private SimplePojo createdByPrototypeFactory;
+
+	@Autowired
+	private SimplePojo createdByStaticFactory;
+
+	public void printFields() {
+		out.println("--- autowired fields ---");
+		out.println(namedInAnnotation);
+		//out.println(createdByPrototypeFactory);
+		out.println(createdByStaticFactory);
+		out.println(simplePojo);
+		out.println("------");
+	}
 }
--- src/main/java/net/demo/pojo/SimplePojo.java
+++ src/main/java/net/demo/pojo/SimplePojo.java
@@ -1,14 +1,26 @@
 package net.demo.pojo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+import static java.lang.System.out;
+
+@Component("namedInAnnotation")
 public class SimplePojo {
 	private int i = 34;
 
-	@Value("${simplePojo.greeting}")
-	private String s = "Hello";
+	//@Value("${simplePojo.greeting}")
+	private String s;
+
+	// this one is important for @Component bean creation
+	public SimplePojo() {
+		this.s = "created by default constructor";
+		out.println("Creating bean: " + this.s);
+	}
+
+	public SimplePojo(String s) {
+		this.s = s;
+		out.println("Creating bean: " + this.s);
+	}
 
 	@Override
 	public String toString() {
